8af5338385f0b9519c51b81c8ce7cf29ea3cb6eb
Hugotgot <duval.hugo73@gmail.Com>
2018-12-03 19:34:10 +0100

    fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,13 @@ const path   = require('path');
 
 const assets_path = path.resolve('./src');
 const output_path = path.resolve('./public/build');
+const public_path = '/build';
 const js_path     = path.join(assets_path, './js');
 const scss_path   = path.join(assets_path, './scss');
 
 Encore
     .setOutputPath(output_path)
-    .setPublicPath(output_path)
+    .setPublicPath(public_path)
 
     .addEntry('app', [
 	path.join(js_path, '/main.js'),
@@ -20,6 +21,9 @@ Encore
     .enableSingleRuntimeChunk()
     .enableSourceMaps(Encore.isProduction())
     .enableVersioning(false)
+     .configureFilenames({
+        images: 'images/[name].[ext]'
+    })
 ;
 
 let config = Encore.getWebpackConfig();
